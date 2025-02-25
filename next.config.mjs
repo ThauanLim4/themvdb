7f527a3342cd9f1337b734ddcb2918ef4371097e
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2025-02-25 13:12:21 -0300

    mudando o next.config para permitir todo tipo de requisição

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/api/mysql/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+                ],
+            },
+        ];
+    },
     images: {
-        remotePatterns: [{hostname: "image.tmdb.org"}],
+        remotePatterns: [{ hostname: "image.tmdb.org" }],
     }
 };
 
