89a9d8c1ddcf8b516cbe057845d8aac938468d1a
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2025-02-28 09:09:48 -0300

    FIX: atualização do caminho de importação do componente de diálogo

--- src/app/pages/criar_conta/page.jsx
+++ src/app/pages/criar_conta/page.jsx
@@ -9,7 +9,7 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/shadcn/dialog"
 import { ChevronRight, ChevronsRight } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
